9ada584669db72a031dd0e18db6108e1010a12ce
Jeff Pettorino <jpettorino@sgss.com>
2026-04-21 13:26:30 -0400

    add password expiration to qua

--- qua/README.md
+++ qua/README.md
@@ -5,4 +5,4 @@ run the script (as root or with 'sudo', duh) with the name of the user you wish 
 
 1. creates user and home directory, with additional group membership in (wheel)
 2. creates /etc/sudoers.d/<user> with sudo "NOPASSWD" priv's
-
+3. expires the password setting with `chage -d 0`
--- qua/qua.sh
+++ qua/qua.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# quick user add script. security considerations abound. caveat h@x0|2
 # Got root?
 if [[ $EUID -ne 0 ]]; then
     echo "Execute this script as root. Maybe try sudo?"
@@ -18,9 +19,11 @@ useradd -m -G wheel $1
 
 echo ""
 echo "Granting sudo privs..."
-echo "$1 ALL=(root) NOPASSWD:ALL" >/etc/sudoers.d/${1}
+echo "$1 ALL=(root) NOPASSWD:ALL" | tee -a /etc/sudoers.d/${1}
 
 echo ""
 echo "Don't forget to set a password."
+chage -d 0 $1
+
 echo "All done."
 echo ""
